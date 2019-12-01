592801e667de49123fb7fa50bcf18dddd0ddb49a
aamandawang <54692206+aamandawang@users.noreply.github.com>
2019-12-01 17:02:28 -0500

    Add files via upload

---
+++ project2/js/scripts.js
@@ -0,0 +1,26 @@
+$("#fadeup").click(function(){
+    $(this).css('animation-play-state', 'running');
+    
+})
+
+
+//$(document).scroll(function(){
+//  var scrollP = $(document).scrollTop();
+//    
+//    if (scrollP == 0){
+//        $('p').css('opacity', '.1');
+//    }
+//    
+//    if (scrollP > 0) {
+//        $('p').css('opacity', '1');
+//    }
+//    
+//
+//var scrollCSS = scrollP/10 + "px"
+//
+//    $('p').css('font-size', scrollCSS);
+//})
+
+
+
+
